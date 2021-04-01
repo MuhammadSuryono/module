534832edb077a3ca5ce551ec8d0cce1a6c947868
Muhammad Suryono <msuryono0@gmail.com>
2021-04-01 16:02:46 +0700

    Add exception for handling error

---
+++ exception/exception.go
@@ -0,0 +1,31 @@
+package exception
+
+type TryCatch struct {
+	Try func()
+	Catch func(Exception)
+	Finally func()
+}
+
+type Exception interface {
+
+}
+
+func (tcf TryCatch) Do()   {
+	if tcf.Finally != nil {
+		defer tcf.Finally()
+	}
+
+	if tcf.Catch != nil {
+		defer func() {
+			if r := recover(); r != nil {
+				tcf.Catch(r)
+			}
+		}()
+	}
+	tcf.Try()
+}
+
+func Throw(up Exception)  {
+	panic(up)
+}
+
